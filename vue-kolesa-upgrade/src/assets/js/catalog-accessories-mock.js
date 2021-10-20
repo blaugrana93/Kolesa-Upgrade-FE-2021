68e492654da10083157711751fd28d19eec86dba
blaugrana93 <arafat_blaugrana@mail.ru>
2021-10-21 01:54:42 +0600

    Перенести главную страницу и модалку на App.vue

---
+++ vue-kolesa-upgrade/src/assets/js/catalog-accessories-mock.js
@@ -0,0 +1,44 @@
+const catalogAccessories = [
+    {
+        id:     1,
+        images: ['t-shirt.jpg', 't-shirt.jpg', 't-shirt.jpg'],
+        score:  '220 баллов',
+        title:  'Футболка Эволюционируй или много текста много текста',
+        desc:   'Брендированная толстовка от Qazaq Republic. Материал: Хлопок 80%,Вискоза 20%',
+        size:   ['S', 'M', 'L'],
+        new:    true,
+        colors: {
+            blue:  'Синий',
+            beige: 'Бежевый',
+            grey:  'Серый',
+        },
+    },
+    {
+        id:     2,
+        images: ['t-shirt.jpg', 't-shirt.jpg', 't-shirt.jpg'],
+        score:  '220 баллов',
+        title:  'Футболка Эволюционируй или много текста много текста',
+        desc:   'Брендированная толстовка от Qazaq Republic. Материал: Хлопок 80%,Вискоза 20%',
+        size:   ['S', 'M', 'L'],
+        new:    true,
+        colors: {
+            blue:  'Синий',
+            beige: 'Бежевый',
+            grey:  'Серый',
+        },
+    },
+    {
+        id:     3,
+        images: ['t-shirt.jpg', 't-shirt.jpg', 't-shirt.jpg'],
+        score:  '220 баллов',
+        title:  'Футболка Эволюционируй или много текста много текста',
+        desc:   'Брендированная толстовка от Qazaq Republic. Материал: Хлопок 80%,Вискоза 20%',
+        size:   ['S', 'M', 'L'],
+        new:    false,
+        colors: {
+            blue:  'Синий',
+            beige: 'Бежевый',
+            grey:  'Серый',
+        },
+    },
+];
---
+++ vue-kolesa-upgrade/src/components/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="myModal" class="modal">
+        <div class="modal__wrapper">
+            <div class="modal__inside"></div>  
+            <span class="icon-close close">
+                <img src="/image/icon-close.svg" alt="icon-close">
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+}
+</script>
+
+<style scoped>
+</style>
