77baaeecd9fdddc4be61f3ac96c89d50065b8bd3
blaugrana93 <arafat_blaugrana@mail.ru>
2021-11-01 01:17:05 +0600

    add product component

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,101 @@
 <template>
-  <div class="wrapper">
-    <div class="content-wrapper">
-      <div class="container">
-        <HeaderVue />
-        <div class="main">
-          <MenuVue />
-          <div class="content">
-            <BannerVue />
-            <FrameVue />
-            <div class="row">
-              <div class="col-12">
-                <div class="filter">
-                  <button
-                    class="filter__button filter__button-active"
-                    type="button"
-                    data-filter="all"
-                  >
-                    Все товары
-                  </button>
-                  <button
-                    class="filter__button"
-                    type="button"
-                    data-filter="clothes"
-                  >
-                    Одежда
-                  </button>
-                  <button
-                    class="filter__button"
-                    type="button"
-                    data-filter="accessories"
-                  >
-                    Акксесуары
-                  </button>
-                </div>
-              </div>
-            </div>
+  <div id="app">
+    <div class="wrapper">
+      <div class="content-wrapper">
+        <div class="container">
+          <HeaderVue />
+          <div class="main">
+            <MenuVue />
 
-            <div class="product__catalog row" id="app"></div>
+            <div class="content">
+              <BannerVue />
+              <FrameVue />
+
+              <FilterVue
+                @showAllBtn="showAllBtn"
+                @showClothesBtn="showClothesBtn"
+                @showAccessesBtn="showAccessesBtn"
+              ></FilterVue>
+
+              <ProductVue
+                :clothes="clothesFromBE"
+                :accesses="accessesFromBE"
+                :selectedTab="selectedTab"
+                @orderFromCard="orderToUser"
+                :search="search"
+              ></ProductVue>
+            </div>
           </div>
         </div>
       </div>
+      <FooterVue />
     </div>
-    <FooterVue />
   </div>
 </template>
 
 <script>
+import axios from "@/axios";
 import HeaderVue from "@/components/Header.vue";
-import MenuVue   from "@/components/Menu.vue";
+import MenuVue from "@/components/Menu.vue";
 import BannerVue from "@/components/Banner.vue";
-import FrameVue from  "@/components/Frame.vue";
+import FrameVue from "@/components/Frame.vue";
 import FooterVue from "@/components/Footer.vue";
-
+import ProductVue from "@/components/Product.vue";
+import FilterVue from "@/components/Filter.vue";
 export default {
   name: "App",
+  mounted() {
+    axios.get("templates/-_RLsEGjof6i/data").then((response) => {
+      this.clothesFromBE = response.data;
+    });
+    axios.get("templates/q3OPxRyEcPvP/data").then((response) => {
+      this.accessesFromBE = response.data;
+    });
+  },
+  data() {
+    return {
+      isShowModal: false,
+      selectedTab: 0,
+      orderPrice: 0,
+      search: "",
+      clothesFromBE: [],
+      accessesFromBE: [],
+      userFromBE: {},
+    };
+  },
+  computed: {
+    allProducts() {
+      return { ...this.clothesFromBE, ...this.accessesFromBE };
+    },
+  },
+  methods: {
+    showAllBtn() {
+      this.selectedTab = 0;
+    },
+    showClothesBtn() {
+      this.selectedTab = 1;
+    },
+    showAccessesBtn() {
+      this.selectedTab = 2;
+    },
+    getScore(scoreFromUserFromBE) {
+      this.orderPrice = scoreFromUserFromBE;
+    },
+    orderToUser(cost) {
+      this.orderPrice -= cost;
+    },
+    searchInApp(searchStr) {
+      this.search = searchStr;
+    },
+  },
   components: {
     HeaderVue,
     MenuVue,
     BannerVue,
     FrameVue,
     FooterVue,
+    ProductVue,
+    FilterVue,
   },
 };
 </script>
---
+++ src/components/Filter.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="filter">
+        <input
+          type="radio"
+          name="type"
+          id="all-goods"
+          value="0"
+          v-model="selectedType"
+          @click="showAll"
+          checked
+        />
+        <label data-key="all-goods" class="filter__button" for="all-goods"
+          >Все товары</label
+        >
+
+        <input
+          type="radio"
+          name="type"
+          id="clothes"
+          value="1"
+          v-model="selectedType"
+          @click="showClothes"
+        />
+        <label data-key="clothes" class="filter__button" for="clothes"
+          >Одежда</label
+        >
+
+        <input
+          type="radio"
+          name="type"
+          id="accesses"
+          value="2"
+          v-model="selectedType"
+          @click="showAccesses"
+        />
+        <label data-key="accesses" class="filter__button" for="accesses"
+          >Аксессуары</label
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Filter",
+  data() {
+    return {
+      selectedType: 0,
+      types: [
+        {
+          name: "All goods",
+          value: "all",
+        },
+        {
+          name: "Clothes",
+          value: "clothes",
+        },
+        {
+          name: "Accessories",
+          value: "accesses",
+        },
+      ],
+    };
+  },
+  methods: {
+    showAll() {
+      this.$emit("showAllBtn");
+    },
+    showClothes() {
+      this.$emit("showClothesBtn");
+    },
+    showAccesses() {
+      this.$emit("showAccessesBtn");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
